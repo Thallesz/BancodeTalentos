5e00e2364634c5fc8e0c7286df301ef7ca3343c3
LAB201 <LAB201@SERVEDUC.COM>
2023-02-01 21:41:13 -0300

    tela de CadastroVaga

--- TccBancoTalentos/TccBancoTalentos/TelaCadastroVaga/CadastroVaga.aspx
+++ TccBancoTalentos/TccBancoTalentos/TelaCadastroVaga/CadastroVaga.aspx
@@ -1,4 +1,27 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CadastroVaga.aspx.cs" Inherits="TccBancoTalentos.TelaCadastroVaga.CadastroVaga" %>
+﻿<%@ Page Title="Cadastro de Vaga" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CadastroVaga.aspx.cs" Inherits="TccBancoTalentos.TelaCadastroVaga.CadastroVaga" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
 
+    <div style="border: solid; border-color: black; padding: 60px; margin-top: 110px; border-radius: 50px">
+        <div class="row">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-3">
+                    <h2 style="font-size: 25px; color: white">Vaga:</h2>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="txtVaga" Style="text-align: center; border-radius: 50px"></asp:TextBox>
+                    <h2 style="font-size: 25px; color: white">Salario:</h2>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="txtSalario" Style="text-align: center; border-radius: 50px"></asp:TextBox>
+                    <h2 style="font-size: 25px; color: white">CargaH:</h2>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="txtCargaH" Style="text-align: center; border-radius: 50px"></asp:TextBox>
+                </div>
+                <div class="col-sm-2">
+                    <asp:Button runat="server" ID="btnRegistar" CssClass="btn btn-success" Text="Registrar" Style="margin-left: 10px; border-radius: 50px; margin-top: 320px" Font-Size="20px" Width="150px" Height="40px" />                    
+                </div>
+                <div class="col-sm-4">
+                    <h2 style="font-size: 25px; color: white">Requisito:</h2>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="txtRequisito" Style="text-align: center; border-radius: 50px"></asp:TextBox>
+                    <h2 style="font-size: 25px; color: white">Empresa:</h2>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="txtEmpresa" Style="text-align: center; border-radius: 50px"></asp:TextBox>
+                </div>
+        </div>
+    </div>
 </asp:Content>
--- TccBancoTalentos/TccBancoTalentos/TelaCadastroVaga/CadastroVaga.aspx.designer.cs
+++ TccBancoTalentos/TccBancoTalentos/TelaCadastroVaga/CadastroVaga.aspx.designer.cs
@@ -11,5 +11,59 @@ namespace TccBancoTalentos.TelaCadastroVaga {
     
     
     public partial class CadastroVaga {
+        
+        /// <summary>
+        /// Controle txtVaga.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtVaga;
+        
+        /// <summary>
+        /// Controle txtSalario.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSalario;
+        
+        /// <summary>
+        /// Controle txtCargaH.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCargaH;
+        
+        /// <summary>
+        /// Controle btnRegistar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegistar;
+        
+        /// <summary>
+        /// Controle txtRequisito.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRequisito;
+        
+        /// <summary>
+        /// Controle txtEmpresa.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmpresa;
     }
 }

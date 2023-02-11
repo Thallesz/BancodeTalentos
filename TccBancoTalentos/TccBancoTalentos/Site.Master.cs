fe576fde4d3a14e169adb7d65e01a812bb299b6c
LAB201 <LAB201@SERVEDUC.COM>
2023-02-10 21:20:48 -0300

    TelaBuscaEmpregos Front e Back End

--- TccBancoTalentos/TccBancoTalentos/Site.Master.cs
+++ TccBancoTalentos/TccBancoTalentos/Site.Master.cs
@@ -9,6 +9,7 @@ namespace TccBancoTalentos
 {
     public partial class SiteMaster : MasterPage
     {
+        public static string ConnectionString = "Server=127.0.0.1;User ID=root;Password=;DataBase=bancotalentos";
         protected void Page_Load(object sender, EventArgs e)
         {
 
---
+++ TccBancoTalentos/TccBancoTalentos/TelaBusca/VisualizarEmprego.aspx.cs
@@ -0,0 +1,58 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TccBancoTalentos
+{
+    public partial class VisualizarEmprego : System.Web.UI.Page
+    {
+        private MySqlConnection connection;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            connection = new MySqlConnection(SiteMaster.ConnectionString);
+        }
+
+        protected void btnBuscaEM_Click(object sender, EventArgs e)
+        {
+            DataTable empregos = new DataTable();
+            empregos.Columns.Add("vaga");
+            empregos.Columns.Add("salario");
+            empregos.Columns.Add("cargah");
+            empregos.Columns.Add("requisitos");
+            empregos.Columns.Add("empresa");
+
+            connection.Open();
+
+            var comando = new MySqlCommand($"SELECT vaga,salario,cargah,requisito,empresa from vagasdisponiveis", connection);
+
+            var reader = comando.ExecuteReader();
+            while (reader.Read())
+            {
+                var linha = empregos.NewRow();
+                linha["vaga"] = reader.GetString("vaga");
+                linha["salario"] = reader.GetFloat("salario");
+                linha["cargah"] = reader.GetTimeSpan("cargah");
+                linha["requisitos"] = reader.GetString("requisito");
+                linha["empresa"] = reader.GetString("empresa");
+                empregos.Rows.Add(linha);
+
+            }
+
+            connection.Close();
+            grdEmpregos.DataSource = empregos;
+            grdEmpregos.DataBind();
+
+            }
+
+        protected void grdEmpregos_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+        }
+    }
+}

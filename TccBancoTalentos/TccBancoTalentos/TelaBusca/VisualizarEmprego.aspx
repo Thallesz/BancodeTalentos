7a76c895fe251bed06a7dba99d458040153dcc88
LAB201 <LAB201@serveduc.com>
2023-03-02 22:03:27 -0300

    Tela de Busca para usuários

--- TccBancoTalentos/TccBancoTalentos/TelaBusca/VisualizarEmprego.aspx
+++ TccBancoTalentos/TccBancoTalentos/TelaBusca/VisualizarEmprego.aspx
@@ -7,19 +7,18 @@
         <h2 class="box-title">Empregos:</h2>
         <div class="row">
             <div style="border: solid; border-color: black; padding: 60px; border-radius: 50px">
-                <asp:Button runat="server" ID="btnBuscaEM" OnClick="btnBuscaEM_Click" Text="Buscar" CssClass="button" style="margin-left:450px" />
+                <asp:Button runat="server" ID="btnBuscaEM" OnClick="btnBuscaEM_Click" Text="Buscar" CssClass="button" Style="margin-left: 450px" />
                 <div class="content-box">
-                    <asp:GridView runat="server" ID="grdEmpregos" OnRowCommand="grdEmpregos_RowCommand" AutoGenerateColumns="false" Width="100%" AllowPaging="true" PageSize="20" CssClass="table" Style="color:white">
+                    <asp:GridView runat="server" ID="grdEmpregos" OnRowCommand="grdEmpregos_RowCommand" AutoGenerateColumns="false" Width="100%" AllowPaging="true" PageSize="20" CssClass="table" Style="color: white">
                         <Columns>
                             <asp:BoundField DataField="id" HeaderText="ID" />
                             <asp:BoundField DataField="vaga" HeaderText="VAGAS" />
-                            <asp:BoundField DataField="salario" HeaderText="SAL ARIO" />
+                            <asp:BoundField DataField="salario" HeaderText="SALARIO" />
                             <asp:BoundField DataField="cargah" HeaderText="CARGA HORARIA" />
                             <asp:BoundField DataField="requisitos" HeaderText="REQUISITOS" />
                             <asp:BoundField DataField="empresa" HeaderText="EMPRESA" />
                             <asp:ButtonField ButtonType="Link" CommandName="candidatar" ControlStyle-CssClass="btn btn-primary" Text="Candidatar" />
                         </Columns>
-
                     </asp:GridView>
                 </div>
             </div>
---
+++ TccBancoTalentos/TccBancoTalentos/TelaBusca/VisualizarUsuários.aspx.cs
@@ -0,0 +1,64 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TccBancoTalentos.TelaBusca
+{
+    public partial class VisualizarUsuários : System.Web.UI.Page
+    {
+        private MySqlConnection connection;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            connection = new MySqlConnection(SiteMaster.ConnectionString);
+        }
+
+        protected void grdUsuarios_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+           
+
+        }
+
+        protected void btnBuscaUsuarios_Click(object sender, EventArgs e)
+        {
+            DataTable usuarios = new DataTable();
+            usuarios.Columns.Add("id");
+            usuarios.Columns.Add("nome");
+            usuarios.Columns.Add("formacao");
+            usuarios.Columns.Add("cpf");
+            usuarios.Columns.Add("rg");
+            usuarios.Columns.Add("email");
+            usuarios.Columns.Add("endereco");
+            usuarios.Columns.Add("celular");
+
+            connection.Open();
+
+            var comando = new MySqlCommand($"SELECT id,nome,formacao,cpf,rg,email,endereco,celular FROM usuario", connection);
+
+            var reader = comando.ExecuteReader();
+            while (reader.Read())
+            {
+                var linha = usuarios.NewRow();
+                linha["id"] = reader.GetInt32("id");
+                linha["nome"] = reader.GetString("nome");
+                linha["formacao"] = reader.GetString("formacao");
+                linha["cpf"] = reader.GetString("cpf");
+                linha["rg"] = reader.GetString("rg");
+                linha["email"] = reader.GetString("email");
+                linha["endereco"] = reader.GetString("endereco");
+                linha["celular"] = reader.GetString("celular");
+                usuarios.Rows.Add(linha);
+            }
+
+            connection.Close();
+            Session["tabela"] = usuarios;
+            grdUsuarios.DataSource = usuarios;
+            grdUsuarios.DataBind();
+        }
+    }
+}

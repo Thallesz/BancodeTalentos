1931d441993faa45733e14609ad26210f44fbd82
LAB201 <LAB201@serveduc.com>
2023-03-17 21:20:20 -0300

    Front End tela de contato

---
+++ TccBancoTalentos/TccBancoTalentos/TelaSujestões/Sujestões.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TccBancoTalentos.TelaSujestões
+{
+    public partial class Sujestões : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void droplistSujest_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
